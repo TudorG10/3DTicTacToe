bc6d36fd63ff0bfc3b27e5a8d0814640e3a28a07
Tudor Gurau <tudor@Alexandrus-MacBook-Pro.local>
2019-01-08 10:57:24 -0500

    added basic classes

---
+++ src/Backend/Board.java
@@ -0,0 +1,26 @@
+package Backend;
+
+public class Board {
+	private int[][][] gameBoard;
+	private int[][] availableSpot;
+
+	public int[][][] getGameBoard() {
+		return gameBoard;
+	}
+
+	public int getAvailableSpot(int x, int y) {
+		return availableSpot[y][x];
+	}
+
+	public void setAvailableSpot(int x, int y) {
+		this.availableSpot[y][x] = this.availableSpot[y][x] += 1;
+	}
+
+
+	public Board() {
+		int[][][] gameBoard = new int[3][3][3];
+		int[][] availableSpot = new int[3][3];
+
+	}
+
+}
