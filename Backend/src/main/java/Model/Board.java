5f1653a487b512d5e99ee2a7dfa4d48877af8b95
TudorG10 <gurau.tudor@yahoo.com>
2019-01-08 12:45:37 -0500

    added ascii visualization

--- Backend/src/main/java/Model/Board.java
+++ Backend/src/main/java/Model/Board.java
@@ -1,27 +1,50 @@
 package src.main.java.Model;
 
 public class Board {
-	
+
 	private int[][][] gameBoard;
 	private int[][] availableSpot;
 
 	public int[][][] getGameBoard() {
 		return gameBoard;
+	}	
+	public void setGameBoard(int x, int y, int z, int player) {
+		 gameBoard[x][y][z] = player;
 	}
 
 	public int getAvailableSpot(int x, int y) {
-		return availableSpot[y][x];
+		return availableSpot[x][y];
 	}
 
 	public void setAvailableSpot(int x, int y) {
-		this.availableSpot[y][x] = this.availableSpot[y][x] += 1;
+		this.availableSpot[x][y] = this.availableSpot[x][y] += 1;
 	}
 
 
 	public Board() {
-		int[][][] gameBoard = new int[3][3][3];
-		int[][] availableSpot = new int[3][3];
+		this.gameBoard = new int[3][3][3];
+		this.availableSpot = new int[3][3];
 
+	}
+
+	public void print(){
+		for(int k = 0; k < 3; k++) {
+			System.out.println(" "+k+" | 0 | 1 | 2 |");
+			System.out.println("---+---+---+---+");
+			for (int i = 0; i < 3; i++){
+				System.out.print( " " + i + " |");
+				for (int j = 0; j < 3; j++){
+					if (gameBoard[j][i][k] == 0){
+						System.out.print("   |");
+					}
+					else{
+						System.out.print(" "+ gameBoard[j][i][k]+" |");
+					}
+				}
+				System.out.println("\n---+---+---+---+");
+			}
+			System.out.println();
+		}
 	}
 
 }
--- Backend/src/main/java/Play/Play.java
+++ Backend/src/main/java/Play/Play.java
@@ -1,5 +1,32 @@
 package src.main.java.Play;
 
+import java.util.Scanner;
+
+import src.main.java.Model.Board;
+
 public class Play {
 	
+	public static void main(String[] args) {
+		int currentPlayer;
+		int turn = -1;
+		
+		Board gameBoard = new Board();
+
+		gameBoard.print();
+		
+		while(true) {
+			
+			turn++;
+			currentPlayer = turn % 2 + 1;
+			System.out.println("Player " + currentPlayer + "'s turn\n");
+			Scanner reader = new Scanner(System.in);  // Reading from System.in
+			System.out.println("Enter a row: ");
+			int x = reader.nextInt(); // Scans the next token of the input as an int.
+			System.out.println("Enter a column: ");
+			int y = reader.nextInt(); // Scans the next token of the input as an int.
+			
+			System.out.println("piece added: " + x + "," + y);
+
+		}
+	}
 }
